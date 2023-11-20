d9a4b8835778bfe23fcbd54c9b685e718dcb25f6
Dovidas <70913423+Dovias@users.noreply.github.com>
2023-11-20 14:00:32 +0200

    add userscript

---
+++ user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name        No ad break servers-minecraft.net
+// @supportURL  https://github.com/dovias
+// @include     /^https?:\/\/servers-minecraft.net\/landing.+$/
+// @grant       none
+// @version     1.0
+// @author      Dovias
+// @description Allows you to bypass 15 second ad break in server-minecraft.net website while voting.
+// ==/UserScript==
+
+const urlParams = new URLSearchParams(window.location.search)
+
+post(urlParams.get('referer'), {
+  id: urlParams.get('id'),
+  username: urlParams.get('username'),
+  post_review: urlParams.get('post_review'),
+  review_score: urlParams.get('review_score'),
+  review_text: urlParams.get('review_text'),
+  verified: urlParams.get('verified')
+})
